5ecb200d93013930e100f90d452f0260ae1d17f4
Robin de Vries <robin@celp.nl>
2015-12-30 17:06:53 +0100

    Its not a syllabus anymore

--- resources/views/pages/voorbeeldcode.php
+++ resources/views/pages/voorbeeldcode.php
@@ -3,4 +3,4 @@
 <p>Om je opweg te helpen met het webprogrammeren is er voorbeeldcode beschikbaar.</p>
 <p>Deze code kun je raadplegen op <?php echo link_to('http://'.env('WEBDB_EXAMPLES'),env('WEBDB_EXAMPLES')); ?>.
     Je kunt door de mappen en bestanden bladeren en de broncode bekijken.
-    Het wachtwoord voor deze website kun je vinden in de syllabus.</p>
+    Het wachtwoord voor deze website kun je vinden in de cursushandleiding.</p>
