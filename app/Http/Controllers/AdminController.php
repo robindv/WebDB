b5032fdacab03b5863fad1fe0f41f07b4c21d9cc
Robin de Vries <robin@celp.nl>
2019-01-05 23:08:26 +0100

    Cleanup

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,13 +11,6 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
 
-    private $connector;
-
-    function __construct()
-    {
-        $this->connector = new CloudStackConnector();
-    }
-
     function getServers()
     {
         $data['servers'] = Server::orderBy('name')->with('group')->where('course_id', \Auth::user()->current_course)->get();
@@ -47,41 +40,6 @@ class AdminController extends Controller
 
         return redirect()->back();
     }
-
-    function getConfig()
-    {
-        $data['zones'] = [];
-        $data['networks'] = [];
-        $data['serviceofferings'] = [];
-        $data['templates'] = [];
-        foreach($this->connector->list_zones()->listzonesresponse->zone as $zone)
-            $data['zones'][$zone->id] = $zone->name;
-        foreach($this->connector->list_templates("executable")->listtemplatesresponse->template as $template)
-            $data['templates'][$template->id] = $template->name." (".$template->displaytext.", ".$template->ostypename.")";
-        foreach($this->connector->list_networks()->listnetworksresponse->network as $network)
-            $data['networks'][$network->id] = $network->name. " (".$network->displaytext.")";
-        foreach($this->connector->list_service_offerings()->listserviceofferingsresponse->serviceoffering as $serviceoffering)
-            $data['serviceofferings'][$serviceoffering->id] = $serviceoffering->name. " (".$serviceoffering->displaytext.")";
-
-        $data['current_zoneid']    = Configuration::where('name','cloudstack_zoneid')->first()->value;
-        $data['current_networkid'] = Configuration::where('name','cloudstack_networkid')->first()->value;
-        $data['current_serviceofferingid'] = Configuration::where('name','cloudstack_serviceofferingid')->first()->value;
-        $data['current_templateid'] = Configuration::where('name','cloudstack_templateid')->first()->value;
-
-        return view('layout')->nest('page','admin.config',$data);
-    }
-
-    function postConfig(Request $request)
-    {
-        Configuration::where('name','cloudstack_zoneid')->first()->fill(['value' => $request->get('cloudstack_zoneid')])->save();
-        Configuration::where('name','cloudstack_networkid')->first()->fill(['value' => $request->get('cloudstack_networkid')])->save();
-        Configuration::where('name','cloudstack_serviceofferingid')->first()->fill(['value' => $request->get('cloudstack_serviceofferingid')])->save();
-        Configuration::where('name','cloudstack_templateid')->first()->fill(['value' => $request->get('cloudstack_templateid')])->save();
-
-
-        return redirect()->back();
-    }
-
 
 
 }
